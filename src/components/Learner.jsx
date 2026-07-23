9922a028a4dc02f20a97f0bfe01e80333841bca4
ssumiran <ssumiran@yahoo.com>
2026-07-22 22:58:50 -0400

    Fix Unique key

--- src/components/Learner.jsx
+++ src/components/Learner.jsx
@@ -1,13 +1,15 @@
 import Score from "./Score";
 
 export default function Learner(props) {
-console.log(props.score)
+//console.log(props.score)
     return (
         <div>
-            <label>Name: {props.name}</label>
-            <label>Bio: {props.bio}</label>
+            <label>Name: {props.name}</label> <br />
+            <label>Bio: {props.bio}</label> <br />
             {props.score.map((s) =>
+                <div key={s.date}>
                 <Score date={s.date} score={s.score} />
+                </div>
             )}
         </div>
     );
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -2,8 +2,8 @@ export default function Score(props) {
 
     return (
         <div>
-            <label>Date: {props.date}</label>
-            <label>Date: {props.score}</label>
+            <label>Date: {props.date}</label> <br />
+            <label>Date: {props.score}</label> <br />
         </div>
     );
 }
