fd28d0a50949131aa986e9e56bb989787d232a8c
ssumiran <ssumiran@yahoo.com>
2026-07-22 23:00:36 -0400

    Fix text issue

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -3,7 +3,7 @@ export default function Score(props) {
     return (
         <div>
             <label>Date: {props.date}</label> <br />
-            <label>Date: {props.score}</label> <br />
+            <label>Score: {props.score}</label> <br />
         </div>
     );
 }
